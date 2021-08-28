1f7f9b15cfe4e9e19011db39e1f1f713dbefbfc6
amelieberry <aac.berry@gmail.com>
2021-08-28 16:36:43 +0400

    Improved rock paper scissors ui and responsiveness

---
+++ README.md
@@ -0,0 +1,4 @@
+A simple user vs computer game of rock, paper, scissors.
+Player makes their choice from the three buttons and computer randomly generates its own choice.
+Five rounds are played before determining if player wins/loses or ties the game.
+A Play Again button allows the player to start again at the end of the five rounds.
--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-// computerPlay function to randomly return rock/paper/scissors
 let tools = ["rock","paper","scissors"];
 let playerScore = 0;
 let computerScore = 0;
 let roundNumber = 0;
+window.devicePixelRatio = 1;
 
+// computerPlay function to randomly return rock/paper/scissors
 function computerPlay() {
     let randTools = tools[Math.floor(Math.random() * tools.length)];
     return randTools.toLowerCase();
 }
-
+// Play round of Rock Paper Scissors
 function playRound(playerSelection) {
+    // Count and display number of rounds
     roundNumber += 1;
-    document.getElementById("roundNumber").innerHTML = roundNumber;
+    document.getElementById("roundNumber").innerHTML = roundNumber + " of 5";
+
+    // Select for Computer
     let computerSelection = computerPlay();
 
-    if (roundNumber == 1) {
-        document.getElementById("finalResult").innerHTML = "";
-    }
-
+    // Assign images based on choices
     document.getElementById("playerChoice").innerHTML = "<img class='innerImages' src=images/" + playerSelection + ".png>";
+    document.getElementById("vs").innerHTML = "<h2>VS</h2>";
     document.getElementById("computerChoice").innerHTML = "<img class='innerImages' src=images/" + computerSelection + ".png>";
 
+    // assess if tie/win/lose based on both choices per round and add to score counter
     if(computerSelection == playerSelection) {
         document.getElementById("result").innerHTML = "We have a tie";
     } else {
@@ -62,7 +65,7 @@ function playRound(playerSelection) {
         }
     }
 
-
+    // when 5 rounds played, end game and announce winner
     if (roundNumber == 5) {
         if(playerScore > computerScore) {
             document.getElementById("finalResult").innerHTML = "You have WON THE GAME!";
@@ -71,8 +74,30 @@ function playRound(playerSelection) {
         } else {
             document.getElementById("finalResult").innerHTML = "You have TIED THE GAME!";
         }
-        computerScore = 0;
-        playerScore = 0;
-        roundNumber = 0;    
+        // remove choice buttons and add replay buttons    
+        document.getElementById("rps-btns").classList.add("btns-visibility");
+        document.getElementById("replay-btn").classList.remove("btns-visibility");
     }
+}
+
+function resetBoard() {
+    // Reset counters
+    computerScore = 0;
+    playerScore = 0;
+    roundNumber = 0;  
+    // Toggle buttons visibility
+    document.getElementById("rps-btns").classList.remove("btns-visibility");
+    document.getElementById("replay-btn").classList.add("btns-visibility");
+    // Reset roundNumber
+    document.getElementById("roundNumber").innerHTML = roundNumber + " of 5";
+    // Reset scoreBoard
+    document.getElementById("playerScore").innerHTML = playerScore;
+    document.getElementById("computerScore").innerHTML = computerScore;
+    // Reset choices
+    document.getElementById("playerChoice").innerHTML = "";
+    document.getElementById("vs").innerHTML = "";
+    document.getElementById("computerChoice").innerHTML = "";
+    // Reset callouts
+    document.getElementById("finalResult").innerHTML = "";
+    document.getElementById("result").innerHTML = "";
 }
